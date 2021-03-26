2db8f3402cd685711b2ad8f5cb8b22a0badce889
Jeekwar <jefrykurniawan11@gmail.com>
2021-03-26 23:19:46 +0700

    mengganti bg big tittle, nambahin section my portfolio sama  Education, juga memberikan effect parallax pada content education

--- script.js
+++ script.js
@@ -2,18 +2,26 @@
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
 
-  $('.jumbotron img').css({
-    transform: `translate(0px, ${wScroll / 4}%)`,
+  $('.isijumbotron').css({
+    transform: `translate(0px, ${wScroll / 5}%)`,
   });
-  $('.jumbotron h1').css({
-    transform: `translate(0px, ${wScroll / 2}%)`,
-  });
-  $('.jumbotron p').css({
-    transform: `translate(0px, ${wScroll / 3}%)`,
-  });
+  // $('.jumbotron h1').css({
+  //   transform: `translate(0px, ${wScroll / 2.5}%)`,
+  // });
+  // $('.jumbotron p').css({
+  //   transform: `translate(0px, ${wScroll / 2.5}%)`,
+  // });
 
   //effect landing element pada project
-  if (wScroll > $('.project').offset().top - 230) {
+  if (wScroll > $('.project').offset().top - 480) {
     $('.project .card').addClass('muncul');
   }
+  if (wScroll > $('.education').offset().top - 430) {
+    $('.education .card.row1').addClass('muncul');
+  }
+  if (wScroll > $('.education').offset().top - 280) {
+    $('.education .card.row2').addClass('muncul');
+  }
 });
+
+// parallax effect landing element saat halaman di buka/refresh
